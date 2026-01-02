64a8ed0e69efa096403a8be03c46ccd78d8bb632
anjanipodomi <anjanidihapsari@gmail.com>
2026-01-02 18:08:53 +0700

    membuat PenyediaViewModel sebagai factory ViewModel untuk menyediakan HomeViewModel dan EntryViewModel dengan repository dari container aplikasi

--- app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,30 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.lifecycle.viewmodel.initializer
+import com.example.myfirebase.repositori.AplikasiDataSiswa
+import com.example.myfirebase.viewmodel.HomeViewModel
+import com.example.myfirebase.viewmodel.EntryViewModel
+
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            AplikasiDataSiswa)
+
+object PenyediaViewModel {
+
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+    }
+}
