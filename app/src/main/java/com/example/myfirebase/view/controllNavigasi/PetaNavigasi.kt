fd99ab9f074f326054c485219393c6f3f746e4ee
anjanipodomi <anjanidihapsari@gmail.com>
2026-01-02 15:29:00 +0700

    menambahkan import yang diperlukan untuk mendukung composable, navigasi antar layar, dan penggunaan HomeScreen serta EntrySiswaScreen dalam aplikasi

--- app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
@@ -1,6 +1,15 @@
 package com.example.myfirebase.view.controllNavigasi
 
-
+import HomeScreen
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.myfirebase.view.EntrySiswaScreen
 
 @Composable
 fun DataSiswaApp(
