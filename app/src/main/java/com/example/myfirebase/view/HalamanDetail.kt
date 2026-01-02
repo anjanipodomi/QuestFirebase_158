568f74164340f0ccc5628b6f7373021ede6c9ee5
anjanipodomi <anjanidihapsari@gmail.com>
2026-01-02 17:55:58 +0700

    menambahkan import yang diperlukan untuk mendukung tampilan HomeScreen, pengelolaan UI dengan Jetpack Compose, penggunaan ViewModel, serta penampilan data siswa dalam aplikasi

--- app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
@@ -1,5 +1,30 @@
 package com.example.myfirebase.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myfirebase.R
+import com.example.myfirebase.modeldata.Siswa
+import com.example.myfirebase.view.route.DestinasiHome
+import com.example.myfirebase.viewmodel.HomeViewModel
+import com.example.myfirebase.viewmodel.PenyediaViewModel
+import com.example.myfirebase.viewmodel.StatusUiSiswa
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
