1f4cb9f3edfb4ae6ff89ebc8e3071d2f8e4c5836
anjanipodomi <anjanidihapsari@gmail.com>
2026-01-09 14:28:03 +0700

    menambahkan import edit view model

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -1,4 +1,19 @@
+@file:OptIn(InternalSerializationApi::class)
+
 package com.example.myfirebase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase.modeldata.DetailSiswa
+import com.example.myfirebase.modeldata.UIStateSiswa
+import com.example.myfirebase.repositori.RepositorySiswa
+import com.example.myfirebase.view.route.DestinasiEdit
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
 
 sealed interface StatusUIEdit {
     object Loading : StatusUIEdit
