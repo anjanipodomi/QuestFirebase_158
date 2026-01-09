2b8580be127aded579bdb67ee4eb20a677c16bc3
anjanipodomi <anjanidihapsari@gmail.com>
2026-01-09 14:23:33 +0700

    membuat EditViewModel untuk mengambil data siswa yang akan diedit

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -5,3 +5,24 @@ sealed interface StatusUIEdit {
     object Success : StatusUIEdit
     object Error : StatusUIEdit
 }
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiEdit.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan")
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    var statusUIEdit: StatusUIEdit by mutableStateOf(StatusUIEdit.Loading)
+        private set
+
+    init {
+        getSiswa()
+    }
+}
+
